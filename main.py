0521e9e139b44d0047d30af9cbe2bfebbfdd1fe1
amys8 <60754183+amys8@users.noreply.github.com>
2021-10-05 18:06:43 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 """Movies Web Scraper
 
-This script allows users to extract information about a movie's ratings and details from IMDb. The data is organized and returned in a comma-separated values file (results.csv). Users should list movie titles, with each movie on a new line, in the
-text file (movies.txt).  
+This script allows users to extract information about a movie's ratings and details from IMDb. 
+The data is organized and returned in a comma-separated values file (results.csv). 
+Users should list movie titles, with each movie on a new line, in the text file (movies.txt).  
 
 This file contains the following functions:
 
